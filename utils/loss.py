f73a836f4e5fc7e2fcfdde4e8dfe02e455967f1c
HaifengXu01 <haifeng.xu@sap.com>
2025-08-16 13:24:21 +0800

    fix loss

--- utils/loss.py
+++ utils/loss.py
@@ -184,7 +184,7 @@ class ComputeLoss:
         na, nt = self.na, targets.shape[0]  # number of anchors, targets
         tcls, tbox, tkpt, indices, anch = [], [], [], [], []
         if self.kpt_label:
-            gain = torch.ones(41, device=targets.device)  # normalized to gridspace gain
+            gain = torch.ones(47, device=targets.device)  # normalized to gridspace gain
         else:
             gain = torch.ones(7, device=targets.device)  # normalized to gridspace gain
         ai = torch.arange(na, device=targets.device).float().view(na, 1).repeat(1, nt)  # same as .repeat_interleave(nt)
