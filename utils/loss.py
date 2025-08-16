52b9584886cba88c1f5ff0d3db5da567ac488d5b
HaifengXu01 <haifeng.xu@sap.com>
2025-08-16 14:37:24 +0800

    1

--- utils/loss.py
+++ utils/loss.py
@@ -116,8 +116,8 @@ class ComputeLoss:
     def __call__(self, p, targets):  # predictions, targets, model
         device = targets.device
         lcls, lbox, lobj, lkpt, lkptv = torch.zeros(1, device=device), torch.zeros(1, device=device), torch.zeros(1, device=device), torch.zeros(1, device=device), torch.zeros(1, device=device)
-        # Adjusted sigmas for CrowdPose's 14 keypoints
-        sigmas = torch.tensor([.26, .25, .25, .35, .35, .79, .79, .72, .72, .62, .62, 1.07, 1.07, 1.07], device=device) / 10.0
+        # Adjusted sigmas for CrowdPose's 13 keypoints
+        sigmas = torch.tensor([.26, .25, .25, .35, .35, .79, .79, .72, .72, .62, .62, 1.07, 1.07], device=device) / 10.0
         tcls, tbox, tkpt, indices, anchors = self.build_targets(p, targets)  # targets
 
         # Losses
