c8207971c12f8b8a7d10ff12b1cecb843636be42
HaifengXu01 <haifeng.xu@sap.com>
2025-08-16 13:19:22 +0800

    change 13-14

--- utils/datasets.py
+++ utils/datasets.py
@@ -361,7 +361,7 @@ class LoadImagesAndLabels(Dataset):  # for training/testing
         self.stride = stride
         self.path = path
         self.kpt_label = kpt_label
-        self.flip_index = [0, 2, 1, 4, 3, 6, 5, 8, 7, 10, 9, 12, 13]  # Updated for CrowdPose's 14 keypoints
+        self.flip_index = [0, 2, 1, 4, 3, 6, 5, 8, 7, 10, 9, 12, 13, 11]  # Updated for CrowdPose's 14 keypoints
 
         try:
             f = []  # image files
